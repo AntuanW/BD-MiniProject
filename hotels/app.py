48928f7e8ceef4c0725355ec4687911d5f1cd6ee
Grzegorz <grzegorzpiskorski971@gmail.com>
2023-05-09 12:22:13 +0200

    Giga logowanie skończone

--- hotels/app.py
+++ hotels/app.py
@@ -1,12 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, session
+from functools import wraps
 import pymongo
 
 
 app = Flask(__name__)
+app.secret_key = b'!yny\x99{\x88,F\x85\x19y\xd67yL'
 
 #Database
 client = pymongo.MongoClient('localhost', 27017)
 db = client.HotelsDBProject
+
+# Decorators
+def login_required(f):
+    @wraps(f)
+    def wrap(*arg, **kwargs):
+        if 'logged_in' in session:
+            return f(*arg, **kwargs)
+        else:
+            return redirect('/')
+    return wrap
 
 #Routes
 from user import routes
@@ -17,5 +29,6 @@ def home():
 
 
 @app.route('/dashboard/')
+@login_required
 def dashboard():
     return render_template('dashboard.html')
--- hotels/user/models.py
+++ hotels/user/models.py
@@ -1,10 +1,16 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, session, redirect
 from passlib.hash import pbkdf2_sha256
 from app import db
 import uuid
 
 
 class User:
+    def start_session(self, user):
+        del user['password']
+        session['logged_in'] = True
+        session['user'] = user
+        return jsonify(user), 200
+
     def signup(self):
         # Create new user object
         user = {
@@ -23,6 +29,22 @@ class User:
             return jsonify({"error": "Email already in use"}), 400
 
         if db.Users.insert_one(user):
-            return jsonify(user), 200
+            return self.start_session(user)
 
         return jsonify({ "error": "Signup failed" }), 400
+    
+    
+    def signout(self):
+        session.clear()
+        return redirect('/')
+    
+    def login(self):
+        user = db.Users.find_one({
+            "email": request.form.get('email')
+        })
+
+        if user and pbkdf2_sha256.verify(request.form.get('password'), user['password']):
+            return self.start_session(user)
+        
+        return jsonify({ "error": "Invalid login data." }), 401
+    
