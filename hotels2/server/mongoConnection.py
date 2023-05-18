ec213a2b0592922acf516298074099843b391e82
antoni <wojcikantek@gmail.com>
2023-05-18 16:27:43 +0200

    server connection

--- hotels2/server/mongoConnection.py
+++ hotels2/server/mongoConnection.py
@@ -1,6 +1,7 @@
 import certifi
 import os
-from pymongo import MongoClient
+from pymongo import *
+from pymongo.database import *
 from dotenv import load_dotenv
 import pprint
 
@@ -12,21 +13,14 @@ password: str = os.getenv("MONGODB_PASSWORD")
 
 class MongoConnection:
     def __init__(self):
-        self.db = None
-        self.client = None
         self.uri = f"""mongodb+srv://{username}:{password}@hotelscluster.53jqjfk.mongodb.net/?retryWrites=true&w=majority"""
-        self.customers = None
-        self.hotels = None
-        self.rooms = None
-
-    def init_mongo_connection(self):
         self.client = MongoClient(self.uri, tlsCAFile=certifi.where())
-        self.db = self.client["HotelsDB"]
-        self.customers = self.db["Customers"]
-        self.hotels = self.db["Hotels"]
-        self.rooms = self.db["Rooms"]
+        self.db: Database = self.client["HotelsDB"]
+        self.customers: Collection = self.db["Customers"]
+        self.hotels: Collection = self.db["Hotels"]
+        self.rooms: Collection = self.db["Rooms"]
 
 
 if __name__ == '__main__':
     mongo = MongoConnection()
-    mongo.init_mongo_connection()
+    print(mongo.db.list_collection_names())
