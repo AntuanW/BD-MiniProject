242f6bb8e0ba22d2635637e8409cac8c14a5ff37
Grzegorz <grzegorzpiskorski971@gmail.com>
2023-06-19 01:51:08 +0200

    Bookings on finish

--- hotels2/routes/views.py
+++ hotels2/routes/views.py
@@ -1,6 +1,7 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, jsonify
 from flask_login import login_required, current_user
 from hotels2.server.dbOperations import *
+import json
 
 views = Blueprint('views', __name__)
 
@@ -16,13 +17,33 @@ def home():
 @views.route('/bookings', methods=['GET', 'POST'])
 @login_required
 def logged_home():
-    return render_template("my_bookings.html", user=current_user)
+    if request.method == 'POST' and request.form.get('checkin') is not None:
+        booking_id = request.form.get('booking_id')
+        customer_id = current_user._id
+
+        date_format = "%Y-%m-%d"
+        new_checkin = request.form.get('new_checkin')
+        new_checkin = datetime.strptime(new_checkin, date_format)
+
+        new_checkout = request.form.get('new_checkout')
+        new_checkout = datetime.strptime(new_checkout, date_format)
+
+        if new_checkout < new_checkin:
+            flash('Check in date must be less or equal than check out date.', category='error')
+        else:
+            # if change_booking():
+            #     flash('Room booked successfully!', category='success')
+            # else:
+            #     flash('Room is already booked in this period of time.', category='error')
+            pass
+
+    my_bookings = get_all_user_bookings(current_user._id)   # str
+    return render_template("my_bookings.html", user=current_user, bookings=my_bookings)
 
 
 @views.route('/rooms')
 def rooms_list():
     rooms = list(mongo.rooms.find())
-    print(rooms)
     return render_template("rooms_list.html", user=current_user, rooms=rooms)
 
 
@@ -38,7 +59,7 @@ def reserve_list():
         check_out = datetime.strptime(check_out, date_format)
         if check_out < check_in:
             flash('Check in date must be less or equal than check out date.', category='error')
-
+        else:
             room_id = request.form.get('room_id')
             customer_id = request.form.get('customer_id')
 
@@ -49,3 +70,16 @@ def reserve_list():
 
     rooms = list(mongo.rooms.find())
     return render_template("reserve_rooms.html", user=current_user, rooms=rooms)
+
+
+@views.route('/remove-booking', methods=['POST'])
+def remove_booking():
+    print('================')
+    print(request.get_json(force=True))
+    booking = request.get_json()
+    booking_id = booking['booking_id']
+    # if remove_specific_booking(booking_id):
+    #     flash('Successfully removed the reservation.', category='success')
+    # else:
+    #     flash('Something went wrong.', category='error')
+    return jsonify({})
--- hotels2/static/index.js
+++ hotels2/static/index.js
@@ -1,3 +1,41 @@
-function book_new_room(){
+function fillAndShowTheForm(hotel, checkin, checkout, booking_id, customer_id){
+    const form = document.querySelector('.rebooking');
 
+    form.innerHTML = '';
+
+    const newFormHTML = `
+      <h2 class="change-title">Change parameters:</h2>
+      <div class="change-params">
+        <h2>${hotel}</h2>
+        <h3>Current check in: ${checkin}</h3>
+        <h3>Current check out: ${checkout}</h3>
+      </div>
+      <div class="form-group">
+        <label for="new_checkin">New check in date:</label>
+        <input type="date" id="new_checkin" name="new_checkin" required>
+      </div>
+      <div class="form-group">
+        <label for="new_checkout">New check out date:</label>
+        <input type="date" id="new_checkout" name="new_checkout" required>
+      </div>
+      <div class="form-group">
+        <input type="hidden" id="booking_id" name="booking_id" value=${booking_id}>
+        <input type="hidden" id="customer_id" name="customer_id" value=${customer_id}>
+      </div>
+      <div class="form-group">
+        <button class="reserve-btn approve-change">Approve change</button>
+      </div>      
+    `;
+
+    form.insertAdjacentHTML('beforeend', newFormHTML);
+}
+
+
+function removeBooking(booking_id) {
+    fetch('/remove-booking', {
+        method: 'POST',
+        body: JSON.stringify({booking_id: booking_id})
+    }).then((_res) => {
+        window.location.href = '/bookings'
+    });
 }
